3f6be80906643be1cc11e357229bb067600b4291
ingobecker <mail@ingobecker.de>
2024-02-12 17:03:06 +0100

    update addOrder() in ShopService to throw a ProdoctNotFoundException

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -7,15 +7,14 @@ public class ShopService {
     private ProductRepo productRepo = new ProductRepo();
     private OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductNotFoundException{
         List<Product> products = new ArrayList<>();
         for (String productId : productIds) {
-            Optional<Product> productToOrder = productRepo.getProductById(productId);
-            if (productToOrder.isEmpty()) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
-            }
-            products.add(productToOrder.get());
+                Optional<Product> productToOrder = productRepo.getProductById(productId);
+                if (productToOrder.isEmpty()) {
+                    throw new ProductNotFoundException("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                }
+                products.add(productToOrder.get());
         }
 
         Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
