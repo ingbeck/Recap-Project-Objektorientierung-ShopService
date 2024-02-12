8499558ba0db88a56b9b26235ff023ab2fb73d1e
ingobecker <mail@ingobecker.de>
2024-02-12 17:17:38 +0100

    add test for updateOrder

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -68,5 +68,20 @@ class ShopServiceTest {
 
     }
 
+    @Test
+    void updateOrder() throws ProductNotFoundException{
+        //GIVEN
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+
+        //WHEN
+        Order actual = shopService.addOrder(productsIds);
+        shopService.updateOrder(actual, OrderStatus.IN_DELIVERY);
+
+        //THEN
+        assertEquals(actual.orderStatus(), OrderStatus.IN_DELIVERY);
+
+    }
+
 
 }
